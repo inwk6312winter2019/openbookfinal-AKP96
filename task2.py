661c4f1b2e34438acca27f18dca103446c4c5d85
AKP96 <ajith.kumarperumal@dal.ca>
2019-02-15 11:21:34 -0800

    Task2

--- task2.py
+++ task2.py
@@ -16,3 +16,24 @@ def my_log():
 
 
 print(my_log())
+
+
+
+
+import operator
+
+def topipaddress():
+    d = dict()
+    fopen = open("access.log","r")
+    for line in fopen:
+        if "GET /" in line or "POST /" in line:
+            ip = line.split('-')[0]
+            if ip not in d:
+                d[ip] = 1
+            else:
+                d[ip] += 1
+    new = sorted(d, key=d.get, reverse=True)[:20]
+    return new
+
+
+print(topipaddress())
